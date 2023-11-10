8a63907b434efbce3d34936a06d92cb7b607e7c9
praiakov <adrianopraia@live.com>
2023-11-09 21:00:18 -0300

    feat: added tests

---
+++ tests/Sunstone.Tests/Domain/GemstoneTests.cs
@@ -0,0 +1,31 @@
+﻿using Sunstone.Domain;
+
+namespace Sunstone.Tests.Domain
+{
+    public class GemstoneTests
+    {
+        [Fact]
+        public void Add_Gemstone_DoesNotThrow()
+        {
+            // Arrange & act
+            var gemstone = new Gemstone(name: "Amethyst", color: Colors.Purple);
+
+            // Assert
+            Assert.NotNull(gemstone);
+        }
+
+        [Theory]
+        [InlineData("Old Burma Ruby")]
+        [InlineData("Axinite")]
+        [InlineData("Black Opal")]
+        public void Add_InvalidColor_ThrowsDomainException(string name)
+            => Assert.Throws<DomainException>(() => new Gemstone(name: name, color: (Colors)18));
+
+        [Theory]
+        [InlineData("", Colors.Orange)]
+        [InlineData(null, Colors.Colourless)]
+        public void Add_GemstoneNameIsNullOrWhiteSpace_ThrowsDomainException(string name, Colors color)
+            => Assert.Throws<DomainException>(() => new Gemstone(name: name, color: color));
+
+    }
+}
---
+++ tests/Sunstone.Tests/UseCases/CreateGemstoneTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Sunstone.Application;
+using Sunstone.Domain;
+
+namespace Sunstone.Tests.UseCases
+{
+    public class CreateGemstoneTests
+    {
+        [Fact]
+        public async Task CreateGemstone_ValidInput_CreatesGemstoneAndReturnsId()
+        {
+            // Arrange
+            var gemstone = new Gemstone("TestGem", Colors.Red);
+            var repositoryMock = new Mock<IRepository>();
+            repositoryMock.Setup(repo => repo.Create(It.IsAny<Gemstone>())).Returns(Task.CompletedTask);
+
+            var createGemstoneUseCase = new CreateGemstoneUseCase(repositoryMock.Object);
+
+            // Act
+            var result = await createGemstoneUseCase.CreateGemstone(new CreateInputModel
+            {
+                Name = gemstone.Name,
+                Colors = gemstone.Color
+            });
+
+            // Assert
+            repositoryMock.Verify(repo => repo.Create(It.Is<Gemstone>(g => g.Name == gemstone.Name && g.Color.Equals(gemstone.Color))), Times.Once());
+            Assert.Equal(Guid.Empty, result);
+        }
+    }
+}
