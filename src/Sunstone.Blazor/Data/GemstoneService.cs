c5fd71217f8c5894642010be1572319e61703696
praiakov <adrianopraia@live.com>
2023-11-15 19:58:55 -0300

    feat: add blazor

---
+++ src/Sunstone.Blazor/Data/GemstoneService.cs
@@ -0,0 +1,55 @@
+﻿using Sunstone.Application;
+using Sunstone.Domain;
+
+namespace Sunstone.Blazor.Data
+{
+    public class GemstoneService
+    {
+        private readonly HttpClient _httpClient;
+        private const string POST_URI = "gemstone";
+        private const string GET_URI = "gemstones";
+
+        public GemstoneService(HttpClient httpClient)
+            => _httpClient = httpClient;
+
+        public async Task<IEnumerable<Gemstone>> GetGemstoneAsync()
+            => await _httpClient.GetFromJsonAsync<IEnumerable<Gemstone>>(GET_URI);
+
+        public async Task<bool> PostGemstoneAsync(Model model)
+        {
+            var response = await _httpClient.PostAsJsonAsync(POST_URI, new CreateInputModel { Name = model.Name, Colors = GetColorName(model.Color) });
+
+            if (response.IsSuccessStatusCode is false)
+                throw new Exception();
+
+            return true;
+
+        }
+
+        #region Private Methods
+        public Colors GetColorName(string color)
+        {
+            return color switch
+            {
+                "Red" => Colors.Red,
+                "Black" => Colors.Black,
+                "Bicolor" => Colors.BiColor,
+                "Blue" => Colors.Blue,
+                "Brown" => Colors.Brown,
+                "Colourless" => Colors.Colourless,
+                "Green" => Colors.Green,
+                "Yellow" => Colors.Yellow,
+                "Gray" => Colors.Gray,
+                "Purple" => Colors.Purple,
+                "White" => Colors.White,
+                "Multicolor" => Colors.Multicolor,
+                "Pink" => Colors.Pink,
+                "Orange" => Colors.Orange,
+                _ => Colors.All,
+            };
+        }
+
+        #endregion
+
+    }
+}
